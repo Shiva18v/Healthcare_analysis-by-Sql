118002d18e3393dd2321be22d5db00768f429b00
Shivam <146189840+Shiva18v@users.noreply.github.com>
2025-06-11 14:17:54 +0530

    Add files via upload

--- select.sql
+++ select.sql
@@ -1,4 +1,11 @@
-select * from patient
+select*from Appointments
 
+select* from Doctors
 
-select * from Doctors
+select*from patient
+
+select*from Medical_Records
+
+select *from Medication
+
+select* from Billing
